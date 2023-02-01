879269bd35136257e38fbd27a69c560b8acc2705
abdelbasset4 <benatmani1999@gmail.com>
2023-02-01 15:10:56 +0100

    reset some code

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose')
 
 const dbConnection = () => {
-    mongoose.connect("mongodb+srv://abdelbasset:abdelbasset4real@abdelbasset.d7gr8ht.mongodb.net/full-ecommerce?retryWrites=true&w=majority").then(() => {
+    mongoose.connect(process.env.DB_URL).then(() => {
         console.log('Database Connected');
     })
-    // mongoose.connect(process.env.DB_URL).then(() => {
-    //     console.log('Database Connected');
-    // })
 }
 module.exports = dbConnection
