957d30459162a3357401cbe2fe013cfe80510ffc
abdelbasset4 <benatmani1999@gmail.com>
2023-02-01 14:35:46 +0100

    change

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
+
 const dbConnection = () => {
-    mongoose.connect(process.env.DB_URL).then(() => {
+    mongoose.connect("mongodb+srv://abdelbasset:abdelbasset4real@abdelbasset.d7gr8ht.mongodb.net/full-ecommerce?retryWrites=true&w=majority").then(() => {
         console.log('Database Connected');
     })
+    // mongoose.connect(process.env.DB_URL).then(() => {
+    //     console.log('Database Connected');
+    // })
 }
 module.exports = dbConnection
